2327652adf631fdcd608e133d6873083bc99d978
ristovkiril <43649881+ristovkiril@users.noreply.github.com>
2020-06-26 16:08:06 +0200

    shared-kernel domain base

---
+++ shared-kernel/src/main/java/mk/ukim/finki/emt/sharedkernel/domain/base/AbstractEntity.java
@@ -0,0 +1,48 @@
+package mk.ukim.finki.emt.sharedkernel.domain.base;
+
+import org.springframework.data.util.ProxyUtils;
+
+public class AbstractEntity<ID extends mk.ukim.finki.emt.sharedkernel.domain.base.DomainObjectId> implements mk.ukim.finki.emt.sharedkernel.domain.base.IdentifiableDomainObject<ID> {
+
+    private ID id;
+
+    public AbstractEntity(){
+
+    }
+
+    public AbstractEntity(ID id){
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id != null? super.hashCode() : this.id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || !getClass().equals(ProxyUtils.getUserClass(obj))) {
+            return false;
+        }
+
+        var other = (AbstractEntity<?>) obj;
+        return id != null && id.equals(other.id);    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s[%s]", getClass().getSimpleName(), id);
+    }
+
+    @Override
+    public ID id() {
+        return this.id;
+    }
+}
