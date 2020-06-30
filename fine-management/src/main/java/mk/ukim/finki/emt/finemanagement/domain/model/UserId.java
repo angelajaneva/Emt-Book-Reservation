19539805ce3b0f0020ea709f468080f22eb127cf
ristovkiril <43649881+ristovkiril@users.noreply.github.com>
2020-06-30 15:00:49 +0200

    UserId Embeddable
    in Fine-management

--- fine-management/src/main/java/mk/ukim/finki/emt/finemanagement/domain/model/UserId.java
+++ fine-management/src/main/java/mk/ukim/finki/emt/finemanagement/domain/model/UserId.java
@@ -2,6 +2,9 @@ package mk.ukim.finki.emt.finemanagement.domain.model;
 
 import mk.ukim.finki.emt.sharedkernel.domain.base.DomainObjectId;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class UserId extends DomainObjectId {
 
     public UserId(String id) {
