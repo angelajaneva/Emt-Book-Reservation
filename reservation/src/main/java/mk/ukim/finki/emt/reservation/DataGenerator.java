f1da983dccc32079c0819e5157cfbb02d20da9b1
ristovkiril <43649881+ristovkiril@users.noreply.github.com>
2020-07-03 16:34:47 +0200

    Data generator fixed in Reservation

--- reservation/src/main/java/mk/ukim/finki/emt/reservation/DataGenerator.java
+++ reservation/src/main/java/mk/ukim/finki/emt/reservation/DataGenerator.java
@@ -4,29 +4,33 @@ import mk.ukim.finki.emt.reservation.application.BookCatalog;
 import mk.ukim.finki.emt.reservation.application.ReservationCatalog;
 import mk.ukim.finki.emt.reservation.application.form.ReservationForm;
 import mk.ukim.finki.emt.reservation.domain.model.*;
-import mk.ukim.finki.emt.reservation.domain.repository.ReservationRepository;
-import mk.ukim.finki.emt.reservation.port.client.BookCatalogClient;
-import mk.ukim.finki.emt.sharedkernel.domain.identity.Money;
+import mk.ukim.finki.emt.sharedkernel.infra.eventlog.DomainEventLogAppender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
-import java.time.Instant;
 import java.time.LocalDate;
 
 @Component
-public class DataGenerator {
+public class DataGenerator implements CommandLineRunner {
 
-    private final BookCatalog bookRepository;
-    private final ReservationCatalog reservationRepository;
+    @Autowired
+    private BookCatalog bookRepository;
+    @Autowired
+    private ReservationCatalog reservationRepository;
 
-    public DataGenerator(BookCatalog bookRepository, ReservationCatalog reservationRepository) {
-        this.bookRepository = bookRepository;
-        this.reservationRepository = reservationRepository;
-    }
+//    public DataGenerator(BookCatalog bookRepository, ReservationCatalog reservationRepository, ApplicationEventPublisher applicationEventPublisher) {
+//        this.bookRepository = bookRepository;
+//        this.reservationRepository = reservationRepository;
+//    }
 
-    @PostConstruct
-    @Transactional
+
     public void generateData(){
         var books = bookRepository.findAll();
         var reservation = reservationRepository.createReservation(makeReservation(books.get(0).getId(), new UserId("1")));
@@ -37,4 +41,8 @@ public class DataGenerator {
         return new ReservationForm(bookId, userId, new DateTaking(LocalDate.now()));
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        generateData();
+    }
 }
